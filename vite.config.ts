2cb0b6115587ea92d29ba1df4c9998191eb4914d
Aahan Sharma <aahans149@gmail.com>
2025-12-12 00:04:04 +0530

    Add vite-plugin-checker to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: false
+    })
+  ],
 })
